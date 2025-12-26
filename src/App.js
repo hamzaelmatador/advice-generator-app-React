0e4704b9e1b36eb632dd417535a3ad0615dffe88
hamza <hamnaddari@gmail.com>
2025-12-26 21:26:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import iconDice from "./images/icon-dice.svg";
+import divDesc from "./images/pattern-divider-desktop.svg";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const fetchAdvice = () => {
+    setLoading(true);
+    setError(null);
+    // Simulate a delay of 2 seconds before making the API call
+    setTimeout(() => {
+      axios
+        .get("https://api.adviceslip.com/advice")
+        .then((response) => {
+          setAdvice(response.data.slip);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching advice:", error);
+          setError("Failed to fetch advice. Please try again.");
+          setLoading(false);
+        });
+    }, 1000);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        ADVICE {"  "}#{advice.id}
+      </p>
+      <h1>
+        {loading ? (
+          <div className="loading-message">
+            <p>Loading</p>
+            <div className="spinner"></div>
+          </div>
+        ) : error ? (
+          <p className="error">{error}</p>
+        ) : advice.advice ? (
+          advice.advice
+        ) : (
+          "Click on the dice to get a random advice"
+        )}
+      </h1>
+      <img src={divDesc} alt="pattern-divider" id="divider" />
+      <button onClick={fetchAdvice} disabled={loading}>
+        <img src={iconDice} alt="icon-dice" id="dice-icon" />
+      </button>
     </div>
   );
 }
